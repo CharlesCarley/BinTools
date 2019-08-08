a7a95023f5181968ac939c6d615620e7cc1d25f8
snailrose <38305297+snailrose@users.noreply.github.com>
2019-08-08 14:57:39 -0600

    Added missing files

--- Source/PE/skPortableUtils.cpp
+++ Source/PE/skPortableUtils.cpp
@@ -23,114 +23,46 @@
   3. This notice may not be removed or altered from any source distribution.
 -------------------------------------------------------------------------------
 */
-
-#define _CRT_SECURE_NO_WARNINGS
 #include "PE/skPortableUtils.h"
-#include <time.h>
-#include "Utils/skDebugger.h"
-#include "skPrintUtils.h"
+#include "Utils/skString.h"
 
 
 
-void skPortableUtils::printHeader(const COFFHeader& header)
+
+void skPortableUtils::getMachine(const COFFHeader& inf, char dest[], int len)
 {
-    size_t bw;
-    char buf[32];
+    dest[0] = 0;
+    switch (inf.m_machine)
+    {
+    case CMT_AMD64:
+        sk_strncpy(dest, len, "AMD64", 16);
+        break;
+    case CMT_ARM:
+        sk_strncpy(dest, len, "ARM", 16);
+        break;
+    case CMT_ARM64:
+        sk_strncpy(dest, len, "ARM64", 16);
+        break;
+    case CMT_IA64:
+        sk_strncpy(dest, len, "IA64", 16);
+        break;
+    case CMT_I386:
+        sk_strncpy(dest, len, "I386", 16);
+        break;
+    default:
+        sk_strncpy(dest, len, "Not handled", 16);
+        break;
+    }
+}
 
-    skPrintf("  Machine:                    %u\n", header.m_machine);
-    skPrintf("  Section Count:              %u\n", header.m_sectionCount);
 
-    bw = strftime(buf, 32, "%D %r", localtime((time_t*)&header.m_timeDateStamp));
-    if (buf[0] != 0 && bw > 0)
-        skPrintf("  Timestamp:                  %s\n", buf);
+void skPortableUtils::getPlatformId(const COFFOptionalHeaderCommon& inf, char dest[], int len)
+{
+    dest[0] = 0;
+    if (inf.m_magic == COFF_MAG_PE32)
+        sk_strncpy(dest, len, "PE32", 64);
+    else if (inf.m_magic == COFF_MAG_PE64)
+        sk_strncpy(dest, len, "PE32+", 64);
     else
-        skPrintf("  Timestamp:                  %u\n", header.m_timeDateStamp);
-        
-    skPrintf("  Symbol Table Offset:        %u\n", header.m_symbolTableOffset);
-    skPrintf("  Number Of Symbols:          %u\n", header.m_symbolCount);
-    skPrintf("  Optional Header Size:       %u\n", header.m_optionalHeaderSize);
-    skPrintf("  Characteristics:            0x%x\n", header.m_characteristics);
-}
-
-void skPortableUtils::printHeader(const COFFSectionHeader& header)
-{
-    skPrintf("  Name:                       %s\n", header.m_name);
-    skPrintf("  Virtual Size:               %u\n", header.m_virtualSize);
-    skPrintf("  Virtual Address:            0x%x\n", header.m_virtualAddress);
-    skPrintf("  Size of Raw Data:           %u\n", header.m_sizeOfRawData);
-    skPrintf("  Pointer To Raw Data:        0x%x\n", header.m_pointerToRawData);
-    skPrintf("  Relocation Table:           %u\n", header.m_pointerToRelocations);
-    skPrintf("  Relocation Count:           %u\n", header.m_numberOfRelocations);
-    skPrintf("  Line Number Location:       0x%x\n", header.m_pointerToLineNumbers);
-    skPrintf("  Line Number Count:          %u\n", header.m_numberOfLineNumbers);
-    skPrintf("  Characteristics:            0x%x\n", header.m_characteristics);
-    skPrintf("  Size of Header:             %u\n", (SKuint32)sizeof(COFFSectionHeader));
-}
-
-void skPrintDataDir(const char* msg, const COFFDataDirectory& dd)
-{
-    if (dd.m_size)
-        skPrintf("%s0x%x,%u\n", msg, dd.m_virtualAddress, dd.m_size);
-    else
-        skPrintf("%s0\n", msg);
-}
-
-template <typename COFFOptionalHeaderVaryingBase, typename SKuintV>
-void skPortableUtils_printHeader(const COFFOptionalHeader<COFFOptionalHeaderVaryingBase, SKuintV>& header)
-{
-    skPrintf("  ImageBase:                  0x%llx\n", (SKuint64)header.m_imageBase);
-    skPrintf("  Section Alignment:          %u\n", header.m_sectionAlignment);
-    skPrintf("  File Alignment:             %u\n", header.m_fileAlignment);
-    skPrintf("  Operating System Version:   %u.%u\n", header.m_majOpSysVersion, header.m_minOpSysVersion);
-    skPrintf("  Image Version:              %u.%u\n", header.m_majImSysVersion, header.m_minImSysVersion);
-    skPrintf("  Sub System Version:         %u.%u\n", header.m_majSubSysVersion, header.m_minSubSysVersion);
-    skPrintf("  Image Size:                 0x%x,%u\n", header.m_sizeOfImage, header.m_sizeOfImage);
-    skPrintf("  Size of Headers:            %u\n", header.m_sizeOfHeaders);
-    skPrintf("  Checksum:                   %u\n", header.m_checkSum);
-    skPrintf("  Subsystem:                  %u\n", header.m_subsystem);
-    skPrintf("  DLL Characteristics:        0x%llx\n", (SKuint64)header.m_dllCharacteristics);
-    skPrintf("  Stack Reserve Size:         %llu\n", (SKuint64)header.m_sizeOfStackReserve);
-    skPrintf("  Stack Commit Size:          %llu\n", (SKuint64)header.m_sizeOfStackCommit);
-    skPrintf("  Stack Heap Reserve:         %llu\n", (SKuint64)header.m_sizeOfHeapReserve);
-    skPrintf("  Stack Commit Size:          %llu\n", (SKuint64)header.m_sizeOfHeapCommit);
-    skPrintf("  Loader Flags:               0x%x\n", header.m_loaderFlags);
-    skPrintf("  RVA and Size Count:         %u\n\n", header.m_numberOfRvaAndSizes);
-
-
-    skPrintUtils::writeColor(CS_DARKYELLOW);
-    skPrintf("Data Directories\n\n");
-    skPrintUtils::writeColor(CS_LIGHT_GREY);
-
-
-    const COFFDataDirectories &dir = header.m_directories;
-
-
-
-    skPrintDataDir("  Export Table:               ", dir.m_exportTable);
-    skPrintDataDir("  Import Table:               ", dir.m_importTable);
-    skPrintDataDir("  Resource Table:             ", dir.m_resourceTable);
-    skPrintDataDir("  Exception Table:            ", dir.m_exceptionTable);
-    skPrintDataDir("  Certificate Table:          ", dir.m_certificateTable);
-    skPrintDataDir("  Base Relocation Table:      ", dir.m_baseRelocationTable);
-    skPrintDataDir("  Debug Table:                ", dir.m_debug);
-    skPrintDataDir("  Architecture (Reserved):    ", dir.m_architecture);
-    skPrintDataDir("  Global Pointer Register:    ", dir.m_globPtrReg);
-    skPrintDataDir("  Thread Local Storage:       ", dir.m_threadLocalStorage);
-    skPrintDataDir("  Loader Config Table:        ", dir.m_loadConfigTable);
-    skPrintDataDir("  Bound Import Table:         ", dir.m_boundImport);
-    skPrintDataDir("  Import Address Table:       ", dir.m_importAddressTable);
-    skPrintDataDir("  Delay Import Descriptor:    ", dir.m_delayImportDescriptor);
-    skPrintDataDir("  CRT Runtime Header:         ", dir.m_crtRuntimeHeader);
-}
-
-
-void skPortableUtils::printHeader(const COFFOptionalHeader32& header)
-{
-    skPortableUtils_printHeader<COFFOptionalHeaderCommonPE32, SKuint32>(header);
-}
-
-
-void skPortableUtils::printHeader(const COFFOptionalHeader64& header)
-{
-    skPortableUtils_printHeader<COFFOptionalHeaderCommonPE64, SKuint64>(header);
+        sk_strncpy(dest, len, "Unknown Header Magic", 64);
 }
